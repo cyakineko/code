851f5ee0c8c7f3bb8820de47cedca0aa2eda88af
tatsuyacyato <98063568+cyakineko@users.noreply.github.com>
2022-03-04 19:02:08 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,24 @@
 input.onButtonPressed(Button.A, function () {
-    X += -1
+	
 })
 input.onButtonPressed(Button.AB, function () {
-    led.unplot(X, Y)
+    led.unplot(0, Y)
     Y += -3
-    basic.pause(100)
-    led.unplot(X, Y)
+    basic.pause(200)
+    basic.pause(200)
+    led.unplot(0, Y)
     Y += 1
+    basic.pause(200)
     basic.pause(100)
-    led.unplot(X, Y)
+    led.unplot(0, Y)
     Y += 1
-    basic.pause(100)
-    led.unplot(X, Y)
+    basic.pause(200)
+    led.unplot(0, Y)
     Y += 1
 })
-input.onButtonPressed(Button.B, function () {
-    X += 1
-})
+let 障害物: game.LedSprite = null
+let スプライト: game.LedSprite = null
 let Y = 0
-let X = 0
 images.createBigImage(`
     . # # # # . . . . .
     . # . . # . . . . .
@@ -28,7 +28,6 @@ images.createBigImage(`
     `).scrollImage(1, 200)
 let NOy = 0
 NOy = 0
-X = 0
 Y = 3
 basic.showLeds(`
     . . . . .
@@ -38,15 +37,50 @@ basic.showLeds(`
     # # # # #
     `)
 basic.forever(function () {
-    led.plot(X, Y)
-    led.unplot(X + 1, 3)
-    led.unplot(X - 1, 3)
-    led.unplot(X - 1, 3)
-    led.unplot(0, 0)
-    if (X < 0) {
-        X = 0
-    }
-    if (X > 4) {
-        X = 4
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . # .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        # # # # #
+        `)
+})
+basic.forever(function () {
+    led.plot(0, Y)
+})
+basic.forever(function () {
+    スプライト = game.createSprite(0, Y)
+    障害物 = game.createSprite(4, 3)
+    障害物.change(LedSpriteProperty.X, -1)
+    if (スプライト.isTouching(障害物)) {
+        control.waitMicros(6000)
     }
 })
